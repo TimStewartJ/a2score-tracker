a1061266239d2ce21b08a5838937cb624bc5d822
Tim Stewart <timstewartj@gmail.com>
2025-04-25 16:55:30 -0700

    fix: update orientation in web app manifest to landscape

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,6 +11,6 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#1f2937",
     id: "com.a2score.app",
     scope: "/",
-    orientation: "portrait"
+    orientation: "landscape"
   }
 }
